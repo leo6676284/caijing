8a0c911143a438b4d59e721f39141e296ad39260
leo6676284 <38238015+leo6676284@users.noreply.github.com>
2018-12-18 11:22:50 +0800

    caijing

--- main.py
+++ main.py
@@ -1,28 +1,79 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 27 11:39:14 2018
+Created on Thu Oct 11 10:46:06 2018
 
 @author: chris
 """
+import time#加载时间库
+from os import listdir
+#nameList=listdir('caijing/caijing/spiders')
+#print(nameList)
+def timeTransform1(time1):#时间戳转换函数
+    #当time1小于1970时
+    if time1 <= 197001010000:
+        time1 = 197001020000
+    #把时间转为字符串
+    time1 = '%s' %time1
+    #转为时间格式
+    st = time.strptime(time1,'%Y%m%d%H%M')
+    #转为时间戳
+    st = time.mktime(st)
+    return st
 
-import time
-import os
-while True:
-  os.system("scrapy crawl sina")
-  time.sleep(300) #每隔一天运行一次 24*60*60=86400s或者，使用标准库的sched模块
-import sched
-#初始化sched模块的scheduler类
-#第一个参数是一个可以返回时间戳的函数，第二个参数可以在定时未到达之前阻塞。
-schedule = sched.scheduler ( time.time, time.sleep )
-#被周期性调度触发的函数
-def func():
-  os.system("scrapy crawl sina")
-def perform1(inc):
-  schedule.enter(inc,0,perform1,(inc,))
-  func()  # 需要周期执行的函数
-def mymain():
-  schedule.enter(0,0,perform1,(300,))
-if __name__=="__main__":
-  mymain()
-  schedule.run() # 开始运行，直到计划时间队列变成空为止关于cmd的实现方法，本人在单次执行爬虫程序时使用的是 
-cmdline.execute("scrapy crawl sina".split())#但可能因为cmdline是scrapy模块中自带的，所以定时执行时只能执行一次就退出了。
+import os #加载os库
+def fuc(direction,n,jg):#需要运行的函数
+    nameList=listdir(direction)
+    m = len(nameList)
+    for i in range(n):
+        for j in range(0,m-2):#批量运行爬虫
+            name = nameList[j].split('.')[0]
+            os.system("scrapy crawl %s"%name)#运行爬虫sh600291
+        time.sleep(jg)
+#fuc(2,3)
+import sys #加载系统库
+sys.setrecursionlimit(10000)  #修改最大递归次数
+def timesetting(date1,time1,time2,jg,direction,k):#date1程序运行间隔，一般为1天86400秒；time1，time2为两个唤醒时间；k为唤醒后爬虫运行持续时间；jg为爬虫每次运行的时间间隔
+    #获取当前时间的格式
+    now_time =int(time.time())
+    #print(now_time)
+    #求出开盘时间段内循环次数
+    #n = int(120/jg)
+    t1 = int(timeTransform1(time1))#time1时间戳
+    t2 = int(timeTransform1(time2))#time2时间戳
+    if k>t2-t1:
+        print('运行时间必须小于等于唤醒间隔！')
+        return
+
+    if now_time<t1:#当时间小于time1，睡眠直到time1程序唤醒
+        print('%s启动，请稍候！'%time1)
+        slt = t1-time.time()#计算time1和现在时间间隔      
+        time.sleep(slt)#睡眠
+        timesetting(date1,time1,time2,jg,direction,k)#递归
+        return
+    #if now_time == time1 or now_time == time2:#当时间等于time1，time2程序唤醒
+    if now_time in range(t1,t1+k+1):
+        n = int((t1+k-now_time)/jg)#运行次数
+        fuc(direction,n,jg)#批量运行爬虫
+        timesetting(date1,time1,time2,jg,direction,k)#递归
+        return
+    if now_time in range(t2,t2+k+1):
+        n = int((t2+k-now_time)/jg)#运行次数
+        fuc(direction,n,jg)#批量运行爬虫
+        timesetting(date1,time1,time2,jg,direction,k)#递归
+         
+    if now_time>t1+k and now_time<t2:#当时间在time1，time2中间，睡眠直到time2唤醒
+        print('%s会再次启动，请稍候！'%time2)
+        slt = t2-time.time()  #计算time2和现在时间间隔        
+        time.sleep(slt)#睡眠
+        timesetting(date1,time1,time2,jg,direction,k)#递归
+        return
+    else:#当时间大于time2睡眠，直到time1后的date1秒后唤醒
+        st1 =  t1+date1 #下一个time1时间戳      
+        time1 = int(time.strftime('%Y%m%d%H%M',time.localtime(st1)))#转换成当地时间格式
+        
+        st2 = t2+date1#下一个time2时间戳
+        time2 = int(time.strftime('%Y%m%d%H%M',time.localtime(st2)))#转换成当地时间格式
+        
+        timesetting(date1,time1,time2,jg,direction,k)#递归
+        return     
+timesetting(86400,201811010930,201811011300,300,'caijing/spiders',7200)
